354a45c9f7996137ab2af437d1199c806d8d938b
kfalconspb <kfalconspb@tarbal.localdomain>
2018-08-22 23:12:37 -0700

    first commit

---
+++ error.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+
+int main()
+{
+ printf("This program will err.\n");
+ return(0);
+}
+
+
---
+++ goodbye.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+
+int main()
+{
+ printf("Farewell, you ugly toad!\n");
+ return(0);
+}
+
