a54961302ba73700a91a545fa1e5bebb12ea2f7d
kfalconspb <kfalconspb@bollingerusa.com>
2018-08-29 01:34:16 -0700

    blowup

---
+++ blowup1.c
@@ -0,0 +1,20 @@
+#inclue <stdio.h>
+
+int main()
+{
+	char c;
+
+	printf("Would you like you computer to explode?\n");
+	c=getchar();
+	if(c=='N')
+	{
+		printf("Okay, Whew!\n");
+	}
+	else
+	{
+		printf("OK: Configuring computer to explode now.\n");
+		printf("Bye!\n");
+	}
+	return(0);
+}
+
