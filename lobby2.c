821c0057bca93adcc6ea5f9e84468c6f12d9f8ca
kfalconspb <kfalconspb@bollingerusa.com>
2018-08-30 23:05:56 -0700

    lobby2:
    :

--- lobby2.c
+++ lobby2.c
@@ -16,16 +16,21 @@ int main()
 /* Figure out what they typed in. */
 
 	c=getchar();
-	if(c=='1')
-		printf("Beverage\nThat will be $8.00\n");
-	else if(c=='2')
-		printf("Candy\nThat will be $5.50\n");
-	else if(c=='3')
-		printf("Hot dog\nThat will be $10.00\n");
-	else if(c=='4')
-		printf("Popcorn\nThat will be $7.50\n");
-	else
+	switch(c)
 	{
+		case '1':
+		printf("Beverage\nThat will be $8.00\n");
+		break;
+		case '2':
+		printf("Candy\nThat will be $5.50\n");
+		break;
+		case '3':
+		printf("Hot dog\nThat will be $10.00\n");
+		break;
+		case '4':
+		printf("Popcorn\nThat will be $7.50\n");
+		break;
+		default:
 		printf("That is not a proper selection.\n");
 		printf("I'll assume you're just not hungry.\n");
 		printf("Can I help whoever's next?\n");
