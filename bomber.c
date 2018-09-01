054f51ba8dcda8d79adbc39aa2d5bed8e7a119d7
kfalconspb <kfalconspb@bollingerusa.com>
2018-08-31 20:52:04 -0700

    bomber

---
+++ bomber.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+void dropBomb(void); /* prototype */
+
+int main()
+{
+	printf("Press Enter to drop the bomb:");
+	getchar();
+	dropBomb();
+	printf("Yikes!\n");
+	return(0);
+}
+
+void dropBomb()
+{
+	int x;
+
+	for(x=20;x>1;x--)
+	{
+		puts("          *");
+	}
+	puts("        BOOM!");
+}
+
+
